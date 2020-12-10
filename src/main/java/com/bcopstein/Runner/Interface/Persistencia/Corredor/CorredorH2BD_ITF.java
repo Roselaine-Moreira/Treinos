8cf8f0f1850804af1122c3874fc0bbb5fe51885e
Roselaine Moreira <roselaine.moreira@edu.pucrs.br>
2020-12-10 17:32:25 -0300

    Commit teste

--- src/main/java/com/bcopstein/Runner/Interface/Persistencia/Corredor/CorredorH2BD_ITF.java
+++ src/main/java/com/bcopstein/Runner/Interface/Persistencia/Corredor/CorredorH2BD_ITF.java
@@ -8,6 +8,7 @@ import com.bcopstein.Runner.Entidades.Dominio.Corredor;
 import org.springframework.data.repository.CrudRepository;
 
 //String ou Long?
+//String ou Long?
 public interface CorredorH2BD_ITF extends CrudRepository<Corredor, String> {
   List<Corredor> findByCodigoCorredor(String codigoCorredor);
 
