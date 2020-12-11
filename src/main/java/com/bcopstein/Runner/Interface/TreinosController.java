941e30e072a3694ac1e6677dfc4be71c1963dbaa
Roselaine Moreira <roselaine.moreira@edu.pucrs.br>
2020-12-11 15:02:51 -0300

    Ajustes para funcionamento do backend no Postaman.

--- src/main/java/com/bcopstein/Runner/Interface/TreinosController.java
+++ src/main/java/com/bcopstein/Runner/Interface/TreinosController.java
@@ -6,39 +6,44 @@ import java.util.List;
 import com.bcopstein.Runner.CasosDeUso.ControleDeTreinos;
 import com.bcopstein.Runner.Entidades.Dominio.Corredor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/treino")
+@RequestMapping("/treinos")
 public class TreinosController {
-
   private ControleDeTreinos ctrlTreinos;
-  
-  //envia a lista dos corredores
-  @PostMapping("/listaCorredores")
+
+  @Autowired
+  public TreinosController(ControleDeTreinos ctrlTreinos) {
+    this.ctrlTreinos = ctrlTreinos;
+
+  }
+
+  // envia a lista dos corredores
+  @GetMapping("/corredores")
   @CrossOrigin(origins = "*")
-  public Collection<Corredor> listaCorredores() {    
+  public Collection<Corredor> listaCorredores() {
     return ctrlTreinos.listaCorredores();
   }
 
-  //cadastro de Corredor
+  // cadastro de Corredor
   @GetMapping(value = "/corredor")
   public String getOi() {
     return "*******************";
   }
 
-  //exibe os treinos a fazer
+  // exibe os treinos a fazer
   @GetMapping(value = "/treinoAtribuido")
   public String getOiii() {
     return "/*/*/*/*/*";
   }
 
-  //exibe os treinos feitos
+  // exibe os treinos feitos
   @GetMapping(value = "/historico")
   public String getOla() {
     return "88888888888888";
